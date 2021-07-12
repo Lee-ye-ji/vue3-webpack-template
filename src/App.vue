39b748aaeac1f06f13968ded6e89b74e8020f431
Lee-Ye-Ji <dpwl30559@gmail.com>
2021-07-12 15:21:07 +0900

    Create Project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <app>{{ message }}</app>
+  <HelloWorld />  
+</template>
+
+<script>
+import HelloWorld from '~/components/HelloWorld';
+export default {
+    components: {
+        HelloWorld
+    },
+    data(){
+        return{
+            message: 'Hello Vue!!!'
+        }
+    }
+}
+</script>
